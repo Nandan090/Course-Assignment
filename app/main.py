d64261d12c01183bfe32ba36f00892e97d8903c2
Nandan KC <kcnandan090@gmail.com>
2026-01-22 15:07:49 +0200

    update deploy file

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+
+load_dotenv()
 
 app = FastAPI()
 
+API = os.getenv("API")
+print("API FROM ENV =", repr(API))
+
 @app.get("/")
 def read_root():
-    return { "msg": "Hello from Everest region, Nepal!", "v": "0.1" }
+    return {"msg": "Hello from Everest region, Nepal!", "v": "0.1"}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/protected")
+def read_protected(api_key: str):
+    if not API or api_key != API:
+        raise HTTPException(status_code=401, detail="Unauthorized access")
+
+    return {"msg": "Welcome to the protected route!"}
